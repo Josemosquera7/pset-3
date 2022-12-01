890d476f32c767aa2c3b7b686c1015926bd85fb2
Josemosquera7 <je.mosqueral1@uniandes.edu.co>
2022-12-01 15:05:51 -0500

    Create script.R

---
+++ code/script.R
@@ -0,0 +1,9 @@
+# Jose Esteban Mosquera - 202110281
+# David Chaparro - 202023984
+# Santiago Huertas -
+
+# Problem Set 3 - Taller de R estadistica y programacion 
+# Profesor Eduard Martinez
+
+require(pacman)
+p_load(idyverse, rio,estimatr,lmtest,fixest, modelsummary, stargazer)
